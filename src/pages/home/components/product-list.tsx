04aa1dba1e4480fd3ce20e586a75e1ffd5987b83
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2025-07-15 14:50:17 -0300

    Feat: new UI for product card 2

--- src/pages/home/components/product-list.tsx
+++ src/pages/home/components/product-list.tsx
@@ -3,11 +3,16 @@ import ProductCard from "./product-card";
 
 export function ProductList() {
   return (
-    <div className="  w-full  h-full  flex flex-col gap-8  ">
-      <section className="px-10 w-full text-center">
+    <div className="  w-full  h-full  flex flex-col gap-10  ">
+      <section className="w-full text-center">
         <p className="text-3xl uppercase font-anton ">Nuestors productos</p>
+        <p className="text-gray-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+          corporis rem officia consequatur dolores labore nulla quibusdam
+          nesciunt iusto rerum.
+        </p>
       </section>
-      <div className="flex flex-col gap-10   ">
+      <div className="flex flex-col gap-4   ">
         {products.map((product, idx) => (
           <ProductCard key={idx} product={product} />
         ))}
